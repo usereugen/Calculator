f5d61211e779d54fb29379afbb35c8fd46c66158
Smallwhitelight <smallwhitelight28@gmail.com>
2023-02-03 19:49:28 +0200

    list modified

--- script/index.js
+++ script/index.js
@@ -7,3 +7,16 @@ const p2 = document.querySelector('.p2')
 p2.addEventListener('click',() => {
     alert('Hello World !')
 })
+
+const list = document.querySelectorAll('li')
+list.forEach(item=>{
+    item.addEventListener('mouseenter',(ev)=>{
+        const x = Math.floor(Math.random() * 256);
+        const y = Math.floor(Math.random() * 256);
+        const z = Math.floor(Math.random() * 256);
+        const bgColor = "rgb(" + x + "," + y + "," + z + ")";
+        ev.target.style.backgroundColor = bgColor
+    })
+})
+
+
