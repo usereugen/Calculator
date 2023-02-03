06ab74d12c98aec52da2987f35f92d441c00a945
Smallwhitelight <smallwhitelight28@gmail.com>
2023-02-03 19:58:21 +0200

    Fix color error LIST

--- script/index.js
+++ script/index.js
@@ -14,8 +14,8 @@ list.forEach(item=>{
         const x = Math.floor(Math.random() * 256);
         const y = Math.floor(Math.random() * 256);
         const z = Math.floor(Math.random() * 256);
-        const bgColor = "rgb(" + x + "," + y + "," + z + ")";
-        ev.target.style.backgroundColor = bgColor
+        const colorLi = "rgb(" + x + "," + y + "," + z + ")";
+        ev.target.style.color = colorLi
     })
 })
 
